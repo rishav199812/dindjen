0440b4b843eaaeb90bede72bc06e9ed168b2e213
rishav199812 <64005862+rishav199812@users.noreply.github.com>
2022-12-15 17:10:16 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+
+class DataMappingTest(unittest.TestCase):
+
+    @classmethod
+
+    def setUpClass(cls):
+
+        pass
+
+
+
+    def test_data_mapping_with_only_passing_item(self):
+
+        self.assertTrue
+
+    def test_data_mapping_with_only_failing_item(self):
+
+        self.assertFalse
+
+
+
+if __name__ == "__main__":
+
+    unittest.main()
